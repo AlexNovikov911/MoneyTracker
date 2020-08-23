472ffb8d38684d63b08524844400abda33a60a1d
AlexNovikov911 <64025440+AlexNovikov911@users.noreply.github.com>
2020-08-24 07:10:27 +0800

    Add Activity Lifecycle Add text colors and background Add text size

--- app/src/main/java/alexnovikov/tilda/ws/MainActivity.java
+++ app/src/main/java/alexnovikov/tilda/ws/MainActivity.java
@@ -2,12 +2,45 @@ package alexnovikov.tilda.ws;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Activity lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity", "onDestroy");
     }
 }
